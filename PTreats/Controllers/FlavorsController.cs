a6027754d1257de3e72ccd16bc75202343f8e6d1
ChrisRDavila <crossdavila@gmail.com>
2024-02-16 15:19:48 -0800

    Add Get route and view for flavor detail

--- PTreats/Controllers/FlavorsController.cs
+++ PTreats/Controllers/FlavorsController.cs
@@ -48,5 +48,14 @@ namespace PTreats.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
+
+    public ActionResult Details(int id)
+    {
+      Flavor thisFlavor = _db.Flavors
+          .Include(flavor => flavor.JoinEntities)
+          .ThenInclude(join => join.Treat)
+          .FirstOrDefault(flavor => flavor.FlavorId == id);
+      return View(thisFlavor);
+    }
   }
 }
