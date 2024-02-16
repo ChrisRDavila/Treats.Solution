3c9c66dec2b0e0144cd1261fb1d3ddd9cadc43d7
ChrisRDavila <crossdavila@gmail.com>
2024-02-16 14:52:22 -0800

    Add Flavors controller with get route and view for index

---
+++ PTreats/Controllers/FlavorsController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using PTreats.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
+
+namespace PTreats.Controllers
+{
+  [Authorize] 
+  public class FlavorsController : Controller
+  {
+    private readonly PTreatsContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public FlavorsController(UserManager<ApplicationUser> userManager, PTreatsContext db)
+    {
+      _userManager = userManager;
+      _db = db;
+    }
+
+    public async Task<ActionResult> Index()
+    {
+      string userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      ApplicationUser currentUser = await _userManager.FindByIdAsync(userId);
+      List<Flavor> userFlavors = _db.Flavors
+                          .Where(entry => entry.User.Id == currentUser.Id)
+                          .ToList();
+      return View(userFlavors);
+    }
+  }
+}
